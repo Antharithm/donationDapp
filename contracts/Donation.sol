945f4249e686ecc6f43cbb51cd00d64f8633f052
Anthony <antharithm707@gmail.com>
2022-09-06 18:28:21 -0400

    added tests

--- contracts/Donation.sol
+++ contracts/Donation.sol
@@ -2,37 +2,35 @@
 pragma solidity ^0.8.0;
 
 contract Donation {
-    address owner;
-    uint256 totalDonations;
+  address owner;
+  uint256 totalDonations;
 
-    struct Donation {
-        address donor;
-        uint256 amount;
-    }
-    Donation donation;
-    Donation[] donations;
+  struct Donation {
+    address donor;
+    uint256 amount;
+  }
+  Donation donation;
+  Donation[] donations;
 
-    constructor() {
-        owner = msg.sender;
-    }
+  constructor() {
+    owner = msg.sender;
+  }
 
-    receive() external payable {
-        donation = Donation(
-            msg.sender,
-            msg.value
-        );
+  receive() external payable {
+    donation  = Donation(
+      msg.sender,
+      msg.value
+    );
 
-        donations.push(donation);
-        totalDonations += msg.value;
-    }
+    donations.push(donation);
+    totalDonations += msg.value;
+  }
 
-    function getDonations() external view returns (Donation[] memory) {
-        return donations;
-    }
+  function getDonations() external view returns (Donation[] memory) {
+    return donations;
+  }
 
-    // Sum of the total donations that have been made
-    function getTotalDonations() external view returns (uint256) {
-        return totalDonations;
-    }
-
+  function getTotalDonations() external view returns (uint256) {
+    return totalDonations;
+  }
 }
